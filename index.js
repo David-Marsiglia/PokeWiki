5227c015b2324cb40a70e2fe9dc7f0e43f672884
DavidMarsiglia <marsigliadavid810@gmail.com>
2024-03-17 14:12:39 -0500

    add routes

--- index.js
+++ index.js
@@ -1,22 +1,13 @@
 import express, { application } from 'express';
-import { insertar } from './src/controller/RegisterController.js';
-import { login } from './src/controller/LoginController.js';
 import path from 'path';
 import { fileURLToPath } from 'url';
-import session from 'express-session'
+import session from 'express-session';
+import { pokeWikiRouter } from './src/routes/PokeWikiRoutes.js';
+import { usersRouter } from './src/routes/UsersRoutes.js';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-
 const app = express();
-app.use(session({
-    secret: 'secreto',
-    resave: false,
-    saveUninitialized: true,
-    cookie: { secure: false } // Recuerda cambiar a true en producción
-  }));
-
-app.listen('10000')
 
 app.use(express.urlencoded({extended:false}));
 app.use(express.json());
@@ -24,52 +15,22 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, 'src')));
 app.use(express.static(path.join(__dirname, 'src/public')));
 
+app.use(session({
+    secret: 'secreto',
+    resave: false,
+    saveUninitialized: true,
+  })
+);
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname + '/public/index.html'));
-});
+app.use(pokeWikiRouter(__dirname));
+app.use(usersRouter(__dirname));
 
-app.get('/login', (req, res) => {
-    if (req.session.usuarioLogueado) {
-        res.redirect('/');
-      } else {
-        res.sendFile(path.join(__dirname + '/src/public/formLogin.html'));
-      }
-    
-});
-
-app.get('/register', (req, res) => {
-    res.sendFile(path.join(__dirname + '/src/public/formRegister.html'));
-});
-app.get('/home', (req, res) => {
-    if (req.session.usuarioLogueado) {
-        res.sendFile(path.join(__dirname + '/src/public/homePage.html'));
-      } else {
-        res.redirect('/login');
-      }
-    
-});
-app.get('/home-guest', (req, res) => {
-    
-    res.sendFile(path.join(__dirname + '/src/public/homePage.html'));
-    
-});
+app.listen('10000')
 
 
-app.post('/register', insertar);
 
-app.post('/login', login);
-app.get('/poke-detail', (req, res) => {
-    if (req.session.usuarioLogueado) {
-        res.sendFile(path.join(__dirname + '/src/public/pokemonDetail.html'));
-      } else {
-        res.redirect('/login');
-      }
-    
-});
-app.get('/logout', (req, res) => {
-    req.session.destroy(()=>{
-        res.redirect('/')
-    })
-});
+
+
+
+
 
