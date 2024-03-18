c491093f7ab624a61a5844d18ac8c1aaf664ee16
David-Marsiglia <137945339+David-Marsiglia@users.noreply.github.com>
2024-03-18 16:20:08 -0500

    Update dbConector.js
    add Environment Variables

--- model/dbConector.js
+++ model/dbConector.js
@@ -1,11 +1,11 @@
 import mysql from 'mysql';
 const pool = mysql.createPool({
     connectionLimit: 10,
-    host: 'process.env.DB_HOST',
-    user: 'process.env.DB_USER',
-    password: 'process.env.DB_PASSWORD',
-    database: 'process.env.DB_NAME',
-    port: 'process.env.DB_PORT'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT
   });
 
   function connection(callback) {
