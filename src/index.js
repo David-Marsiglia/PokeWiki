df313d31b825429d8f52008ceae7d2a62b40b640
DavidMarsiglia <marsigliadavid810@gmail.com>
2024-03-18 14:42:02 -0500

    master

--- src/index.js
+++ src/index.js
@@ -13,8 +13,8 @@ const app = express();
 app.use(express.urlencoded({extended:false}));
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, '../src')));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static('./src'));
+app.use(express.static('./src/public'));
 
 app.use(session({
     secret: 'secreto',
