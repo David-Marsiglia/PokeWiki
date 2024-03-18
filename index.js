06f9a5cadbf8cadd91b611d0d94df2bea8b02559
DavidMarsiglia <marsigliadavid810@gmail.com>
2024-03-18 15:07:17 -0500

    master

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ import express, { application } from 'express';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import session from 'express-session';
-import { pokeWikiRouter } from './src/routes/PokeWikiRoutes.js';
-import { usersRouter } from './src/routes/UsersRoutes.js';
+import { insertar } from "./controller/RegisterController.js";
+import { login } from "./controller/LoginController.js";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const port = process.env.PORT || 10000;
@@ -13,8 +13,9 @@ const app = express();
 app.use(express.urlencoded({extended:false}));
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, 'src')));
-app.use(express.static(path.join(__dirname, 'src/public')));
+app.use(express.static('./'));
+app.use(express.static('./public'));
+
 
 app.use(session({
     secret: 'secreto',
@@ -23,8 +24,53 @@ app.use(session({
   })
 );
 
-app.use(pokeWikiRouter(__dirname));
-app.use(usersRouter(__dirname));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname + '/public/index.html'));
+});
+
+app.get('/login', (req, res) => {
+  if (req.session.usuarioLogueado) {
+      res.redirect('/');
+    } else {
+      res.sendFile(path.join(__dirname + '/public/formLogin.html'));
+    }
+
+});
+
+app.get('/register', (req, res) => {
+  res.sendFile(path.join(__dirname + '/public/formRegister.html'));
+});
+app.get('/home', (req, res) => {
+  if (req.session.usuarioLogueado) {
+      res.sendFile(path.join(__dirname + '/public/homePage.html'));
+    } else {
+      res.redirect('/login');
+    }
+
+});
+app.get('/home-guest', (req, res) => {
+
+  res.sendFile(path.join(__dirname + '/public/homePage.html'));
+
+});
+
+
+app.post('/register', insertar);
+
+app.post('/login', login);
+app.get('/poke-detail', (req, res) => {
+  if (req.session.usuarioLogueado) {
+      res.sendFile(path.join(__dirname + '/public/pokemonDetail.html'));
+    } else {
+      res.redirect('/login');
+    }
+
+});
+app.get('/logout', (req, res) => {
+  req.session.destroy(()=>{
+      res.redirect('/')
+  })
+});
 
 app.listen(port)
 
