e902ee02adf5b80079b86157a99b8903b3c3e7d8
DavidMarsiglia <marsigliadavid810@gmail.com>
2024-03-18 11:24:18 -0500

    modified

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ import express, { application } from 'express';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import session from 'express-session';
-import { pokeWikiRouter } from './routes/PokeWikiRoutes.js';
-import { usersRouter } from './routes/UsersRoutes.js';
+import { pokeWikiRouter } from './src/routes/PokeWikiRoutes.js';
+import { usersRouter } from './src/routes/UsersRoutes.js';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
@@ -12,8 +12,8 @@ const app = express();
 app.use(express.urlencoded({extended:false}));
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, '../src')));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'src')));
+app.use(express.static(path.join(__dirname, 'src/public')));
 
 app.use(session({
     secret: 'secreto',
